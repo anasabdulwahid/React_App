bbbe5bb97947a42d94b6ef2c1b1e5fabcfc277fc
Anas Abdul <anasabdulwahid@gmail.com>
2023-04-02 13:42:21 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+const App = () => {
+  const name = "John";
+  
+  return (
+    <div className="App">
+      <h1>Hello, {2+2} </h1>
+      {name ? (
+        <>
+          test
+        </>
+      ): (
+        
+          test
+        
+      )}
+    </div>
+  );
+      }
+
+export default App;
